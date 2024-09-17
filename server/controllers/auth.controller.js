c01000ada8c770f4e4aff81478d43b0334d5e079
shiva-manu <shivamani6964@gmail.com>
2024-09-17 18:37:48 +0530

    Added the mailtrap function to it

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,9 +1,13 @@
 import User from "../models/user.module.js";
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 export const signup=async(req,res)=>{
    try{
       const {name,username,email,password}=req.body
       const existingEmail=await User.findOne({email});
+      if(!name || !username || !email || !password){
+         res.status(400).json({message:"All fields are required"});
+      }
       if(existingEmail){
          return res.status(400).json({message:"Email already exist"});
       }
@@ -24,8 +28,24 @@ export const signup=async(req,res)=>{
          username
       });
       await user.save();
+      const token=jwt.sign({userId:user._id},process.env.JWT_SECRET,{expiresIn:"3d"})
+      res.cookie('jwt-linkedin',token,{
+         httpOnly:true, //prevent XSS attack
+         maxAge:3*24*60*1000,
+         sameSite:"strict",
+         secure:process.env.NODE_ENV==="production",//prevents the man-middle attack
+      });
+      res.status(201).json({message:"User registered successfully"});
+      const profileUrl=process.env.CLIENT_URL+"/profile/"+user.username
+      //todo:send welcome email
+      try{
+         await sendWelcomeEmail(user.email,user.name,profileUrl);
+      }catch(emailError){
+         console.log("Error sending Welcome Email",emailError);
+      }
    }catch(error){
-
+      console.log("Error in signUp: ",error.message);
+      res.status(500).json({message:"Internal server error"});
    }
 }
 export const login=(req,res)=>{
